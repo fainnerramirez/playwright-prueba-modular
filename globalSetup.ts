a9b76e4bd97856b4553a6828ea5ec8af87575e51
fainnerramirez <fainnerramirez@gmail.com>
2025-05-14 15:55:10 -0500

    feat: proxy #6

--- globalSetup.ts
+++ globalSetup.ts
@@ -1,24 +1,29 @@
 import fs from 'fs';
 import fetch from 'node-fetch';
-import { chromium } from 'playwright-extra';
-// import stealth from 'playwright-extra-plugin-stealth';
-
-// chromium.use(stealth());
 
 export default async function globalSetup() {
-    const momoProxyUrl = 'https://api.momoproxy.com/ips?cty=any&cnt=20&ft=txt&split=%0A&rp=2&ts=20&key=5nLiUqOK';
+    const proxyListApiUrl =
+        'https://www.proxy-list.download/api/v1/get?type=http&anon=elite&country=US';
 
     try {
-        const response = await fetch(momoProxyUrl);
+        const response = await fetch(proxyListApiUrl);
         const text = await response.text();
-        const proxies = text.trim().split('\n');
-        const selectedProxy = proxies[0];
-        const envContent = `SELECTED_PROXY=http://${selectedProxy}`;
 
-        fs.writeFileSync('.proxy-env', envContent); // Guardamos una variable temporal
+        const proxies = text
+            .trim()
+            .split('\n')
+            .map(p => p.trim())
+            .filter(Boolean);
+
+        const selectedProxy = proxies[0]; // puedes implementar lógica para testear cuál funciona mejor
+
+        if (!selectedProxy) throw new Error('❌ No se encontraron proxies válidos.');
+
+        const envContent = `SELECTED_PROXY=http://${selectedProxy}`;
+        fs.writeFileSync('.proxy-env', envContent);
 
         console.log(`✅ Proxy guardado: ${selectedProxy}`);
     } catch (err) {
-        console.error('❌ No se pudo obtener el proxy:', err);
+        console.error('❌ Error al obtener el proxy:', err);
     }
 }
